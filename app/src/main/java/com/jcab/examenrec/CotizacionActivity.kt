d640ae8b99be21f95c06169870b073bf898d9f48
JuanCAB <2017030691@upsin.edu.mx>
2023-06-09 11:24:26 -0700

    Update 1

---
+++ app/src/main/java/com/jcab/examenrec/CotizacionActivity.kt
@@ -0,0 +1,11 @@
+package com.jcab.examenrec
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CotizacionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cotizacion)
+    }
+}
