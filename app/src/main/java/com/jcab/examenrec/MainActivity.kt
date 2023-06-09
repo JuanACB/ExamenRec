548545ea2adc05d7b042db2df4e55a6938d07d51
JuanCAB <2017030691@upsin.edu.mx>
2023-06-09 11:46:42 -0700

    Update 3

--- app/src/main/java/com/jcab/examenrec/MainActivity.kt
+++ app/src/main/java/com/jcab/examenrec/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.jcab.examenrec
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnCotizar : Button;
+    private  lateinit var txtName : EditText;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCotizar.setOnClickListener {
+            if (txtName.text.toString() == "") {
+                Toast.makeText(this, "Favor de ingresar un nombre", Toast.LENGTH_SHORT).show()
+            } else {
+                startActivity(
+                    Intent(this, CotizacionActivity::class.java)
+                        .putExtra("nombre", txtName.text.toString())
+                )
+            }
+        }
     }
 }
